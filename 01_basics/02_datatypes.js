2a37f5a10471f8f9c3a453958555929ef97843ef
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-02-05 21:26:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let fullName = "ROHIT MAKAM";
+let rollNo = 146;
+let isPresent = true;
+//  string => ""
+// number => 1234
+// boolean => true/false
+// null => object
+// undefind
+// symbol=>unique,
+console.log(typeof 1234);
