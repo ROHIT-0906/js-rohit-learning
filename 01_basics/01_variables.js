e744d80034659a3d7e9830ce97852301a9984231
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-02-05 21:26:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 12345;
+let accountEmail = "ROhit@gamil.com";
+let accountPassword = 12344545;
+accountCity = "Mumbai";
+console.table([accountId , accountEmail , accountPassword , accountCity]);
