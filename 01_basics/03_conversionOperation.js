233c0fb5cc73086b7a5d828eba1311e411305448
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-02-06 16:04:33 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let Score = false
+
+// let accountValue = Number(Score)
+// console.log(typeof (accountValue));
+// console.log(accountValue);
+
+// "33"=> 33
+// "33abc" => NaN
+//  true => 1; false => 0
+//null => 0
+//undefind => undefind
+
+//let isloggedIn = 
+//let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+   // 1 => true; 0 => false
+   // "" => false
+   // 123 => true
+    
+   let someNumber = 33
+   let stringNumber = String(someNumber)
+   console.log(stringNumber);
+
