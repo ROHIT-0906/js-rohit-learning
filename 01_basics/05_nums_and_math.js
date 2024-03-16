ae22728fb4b3ed9cf57732bf12524a9db6d5b121
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-03-16 10:39:03 +0000

    Math&num

---
+++ 01_basics/05_nums_and_math.js
@@ -0,0 +1,26 @@
+// const Score = 400
+// console.log(Score);
+// const gameScore = new Number(400)
+// console.log(gameScore);
+
+// console.log(gameScore.toString().length);
+// console.log(gameScore.toFixed(2)); // convert into value like float
+
+// const otherNumber = 23.897
+// console.log(otherNumber.toPrecision(3)); 
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); // to count zeros
+
+
+//++++++++++++++++++++++ Math +++++++++++++++++++++++
+
+console.log(Math.round(4.9));
+console.log(Math.ceil(6.4));
+console.log(Math.floor(6.8));
+console.log(Math.max(7,8,9,5));
+
+const min =10
+const max = 20
+console.log(Math.floor(Math.random()*((max-min)+1) + min))
+
