cf3b6cac79854c4828f4ff94f8c1c7534d1730fc
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-03-29 18:57:44 +0000

    02_Array

---
+++ 02_basic/02_Array.js
@@ -0,0 +1,22 @@
+const marvel_heros = ["thor","Ironman","Spiderman"]
+const dc_heros = ["Superman","Flash","Batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+// const all_new_heros = [...marvel_heros,...dc_heros] //similar as concat
+// console.log(all_new_heros);
+// const another_Array = [1,2,3,[4,5,6],7,[6,7,8,[9,12,5]]] // this is used for deep Arrays
+// const another_new_Array = another_Array.flat(Infinity)
+// console.log(another_new_Array);
+
+// console.log(Array.isArray("Rohit"));
+// console.log(Array.from("Rohit"));        // convert into Array
+console.log(Array.from({name : "Rohit"}));   
+let Score1 =100
+let Score2 = 200
+let Score3 = 300
+// console.log(Array.of(Score1,Score2,Score3));
+console.log(Array.of(Score1,Score2,Score3));
+
