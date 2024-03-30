20634ab555dbe4fdf96c5f55d7daa7a36f0e6e51
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-03-30 18:58:58 +0000

    04_Objects

---
+++ 02_basic/04_Objects.js
@@ -0,0 +1,31 @@
+// const tinderUser = new Object() this is a singletan object
+const tinderUser = {} // this not a singletan object
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = true
+// console.log(tinderUser);
+const regularUser = {
+    email: "rohit@gmail.com",
+    fullname:{
+        Username:{
+            firstName:"Rohit",
+            lastName:"Makam"
+            
+        }
+
+    }
+}
+// console.log(regularUser.fullname.Username); // nested Objects
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj3 = {4:"c",5:"d"}
+// const obj3 = Object.assign({},obj1,obj2)
+const obj4 = {...obj1,...obj2,...obj3}
+console.log(obj4);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
