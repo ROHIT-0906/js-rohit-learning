8836cbb786929a7a656f3fd4e0e54d74dbc0b909
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-03-30 19:25:05 +0000

    04_Objects

--- 02_basic/04_Objects.js
+++ 02_basic/04_Objects.js
@@ -22,10 +22,18 @@ const obj2 = {3:"a",4:"b"}
 const obj3 = {4:"c",5:"d"}
 // const obj3 = Object.assign({},obj1,obj2)
 const obj4 = {...obj1,...obj2,...obj3}
-console.log(obj4);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(obj4);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+const course ={
+    courseName:"JS in Hindi",
+    coursePrice:999,
+    Instructor:"Rohit"
+}
+const {Instructor: courseInstructor} = course
+// console.log(Instructor);
+console.log(courseInstructor);
 
 
