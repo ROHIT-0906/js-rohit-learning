91aebc0392d9ef31ec0f955262a87b33491116f7
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-06-11 09:22:53 +0000

    four

---
+++ 04_control_flow/four.js
@@ -0,0 +1,15 @@
+const languages={
+    Js: "Javascript",
+    cpp:"c++",
+    Rb:"Ruby",
+    Swift:"Swift byy apple"
+}
+// console.log(languages);
+
+for (const key in languages) {
+//   console.log(`${key} is shortcut for ${languages[key]}`);
+}
+
+// FOR EACH
+
+const coding = ["Js","Ruby","python","Java"]
